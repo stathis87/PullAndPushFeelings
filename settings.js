6edeeefd0f667af8586a77aabe589975a50d28de
sofiataipa <taipa.sofia@gmail.com>
2023-01-20 15:50:09 +0000

    link to p5 instances

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 // const cnv1 = document.getElementById('cnv1');
 // const cnv2 = document.getElementById('cnv2');
-
+// https://editor.p5js.org/caminofarol/sketches/r609C2cs
 
 function setup() {
     let cnv1 = createCanvas(windowWidth, windowHeight);
