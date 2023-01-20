09fefbcaefeb8362db5b8817d77bf1db7052497a
Stathis Doganis <st.doganis@gmail.com>
2023-01-20 18:46:41 +0300

    add scripts and p5

---
+++ settings.js
@@ -0,0 +1,12 @@
+// const cnv1 = document.getElementById('cnv1');
+// const cnv2 = document.getElementById('cnv2');
+
+
+function setup() {
+    let cnv1 = createCanvas(windowWidth, windowHeight);
+    cnv1.parent('cnv1')
+}
+
+function draw() {
+    background(0,255,0);
+}
